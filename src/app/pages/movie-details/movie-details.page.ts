930bc4f0a0ef64b0eb4250591eacd613b4197831
Javi <encriptator3000plus@gmail.com>
2020-12-18 21:25:10 +0100

    show details of each movie added

--- src/app/pages/movie-details/movie-details.page.ts
+++ src/app/pages/movie-details/movie-details.page.ts
@@ -1,15 +1,19 @@
+import { MovieService } from './../../services/movie.service';
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-movie-details',
   templateUrl: './movie-details.page.html',
   styleUrls: ['./movie-details.page.scss'],
 })
 export class MovieDetailsPage implements OnInit {
+  details: object = null;
 
-  constructor() { }
+  constructor(private movieService: MovieService, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
+    let id = this.activatedRoute.snapshot.paramMap.get('id');
+    this.movieService.getDetails(id).subscribe(response => this.details = response);
   }
 
 }
